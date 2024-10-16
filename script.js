1513b2964a85c7e789beacf85118abae99d621c2
labywebowka25 <173664@stud.prz.edu.pl>
2024-10-16 09:11:13 +0000

    2_1.1/1.2

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-(function () {
-  const example = document.getElementById('example')
-  const cw1 = document.getElementById('cw1')
-  const cw2 = document.getElementById('cw2')
-  const cw3 = document.getElementById('cw3')
-  const answer = document.getElementById('answer')
+(async function () {
+  const example = document.getElementById("example");
+  const cw1 = document.getElementById("cw1");
+  const cw2 = document.getElementById("cw2");
+  const cw3 = document.getElementById("cw3");
+  const answer = document.getElementById("answer");
 
-  example.addEventListener("click", function () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(array => {
-        console.log(array)
+  example.addEventListener("click", async function () {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
+        console.log(array);
         answer.innerHTML = JSON.stringify(array);
-      })
-  })
+      });
+  });
 
-  cw1.addEventListener("click", function () {
-    //TODO
-  })
+  cw1.addEventListener("click", async function () {
+    answer.innerHTML = "Loading...";
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
+        console.log(array);
+        answer.innerHTML = JSON.stringify(array);
+      });
+    
+  });
 
   cw2.addEventListener("click", function () {
     //TODO
-  })
+  });
 
   cw3.addEventListener("click", function () {
     //TODO
-  })
-
+  });
 })();
